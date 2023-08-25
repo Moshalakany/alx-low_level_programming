db3c6b177caa81e164f55fc47f79af066e872c3c
Ahmed Mohamed Elshalakany <shalakanyam@gmail.com>
2023-08-25 03:16:38 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t size = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -18,8 +18,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		size++;
+		s++;
 	}
 
-	return (size);
+	return (s);
 }
