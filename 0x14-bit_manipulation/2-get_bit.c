a502b77f3b23fa93673a9c29fe1314132b743c01
Ahmed Mohamed Elshalakany <shalakanyam@gmail.com>
2023-09-01 17:56:56 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * get_bit - get the value of a bit at a given index
+ * get_bit - function to get the value of a bit at a given index
  * @n: number to evaluate
  * @index: index starting from 0, of the bit we want to get
  * Return: Value of bit at index, or -1 if error
