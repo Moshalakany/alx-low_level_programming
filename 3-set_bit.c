ea9ae91dcc190a52e5c8eee85eb7b26ceb81bb55
Ahmed Mohamed Elshalakany <shalakanyam@gmail.com>
2023-09-01 17:57:32 +0300

    Update 3-set_bit.c

--- 3-set_bit.c
+++ 3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - sets value of a bit to 1 at a given index
+ * set_bit - function that sets value of a bit to 1 at a given index
  * @n: decimal number passed by pointer
  * @index: index position to change, starting from 0
  * Return: 1 if it worked, -1 if error
