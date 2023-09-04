c857b7492d70f9f344a7970ff0be0f30e85c043b
Ahmed Mohamed Elshalakany <shalakanyam@gmail.com>
2023-09-05 01:42:03 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  * read_textfile - A function that reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
  * @letters: number of letters it should read and print
- *Author Moshalakany
+ *
  * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
