8b1b97a7c5efbf61ba7ca9dbbccdd50a03fb6ad6
Ahmed Mohamed Elshalakany <shalakanyam@gmail.com>
2023-09-05 01:33:26 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
+ * create_file -Function that Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
